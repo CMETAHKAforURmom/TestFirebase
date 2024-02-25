4b866270f1be4b2ae70169de408ed6c42d43201c
isype <isyperandrei@gmail.com>
2024-02-25 13:37:33 +0700

    Fixing all screens

--- app/src/main/java/ru/test/andernam/domain/old/MessageListObserv.kt
+++
@@ -1,8 +0,0 @@
-package ru.test.andernam.domain.old
-
-import androidx.lifecycle.MutableLiveData
-
-class MessageListObserv : MutableLiveData<MutableList<Message>>() {
-
-
-}
--- app/src/main/java/ru/test/andernam/domain/repository/ProfileInfo.kt
+++
@@ -1,5 +0,0 @@
-package ru.test.andernam.domain.repository
-
-import android.net.Uri
-
-data class ProfileInfo (var name: String, var linkImage: Uri?, var dialogsLink: String?)
